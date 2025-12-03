c54e1828f3f0fcc76272c7142cdf1c48ae6d8f0f
Oleksii113 <olvelykyi23ig@epms.pt>
2025-12-03 11:22:21 +0000

    update

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,11 @@
 import mongoose from "mongoose";
 export async function connectToDataBase() {
+    const uri = process.env.MONGODB_URL;
 
+    if (!uri) {
+        throw new Error(
+            "A Variável de ambiente MONGO_DB não existe."
+        );
+    }
+    const dbName = process.env.MONGODB_DB_NAME || "ficha5_conspirações";
 }
