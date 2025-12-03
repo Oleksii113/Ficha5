a75d5496e2220461667c4f90ca1ae323020aa158
Oleksii113 <olvelykyi23ig@epms.pt>
2025-12-03 11:46:37 +0000

    update

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 // import app from ".app/js";
 import "dotenv/config";
 import { connectToDataBase } from "./src/config/database";
+import { application } from "express";
 
 const PORT = process.env.PORT || 3000;
+
+async function startServer() {
+    try {
+        await connectToDataBase();
+        app.listen(PORT, () => {
+            console.log(`Servidor a correr na porta ${PORT}`);
+        });
+
+    } catch (error) {
+        console.error("ERro Critico ao arrancar o servidor", error);
+        process.exit(1);
+    }
+}
